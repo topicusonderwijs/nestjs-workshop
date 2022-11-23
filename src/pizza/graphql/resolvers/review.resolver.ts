e81022257641e08ba386416bc991076e0330061a
tedroeloffzen <ted.roeloffzen@topicus.nl>
2022-11-23 22:02:29 +0100

    feat: added current user decorator

--- src/pizza/graphql/resolvers/review.resolver.ts
+++ src/pizza/graphql/resolvers/review.resolver.ts
@@ -2,8 +2,9 @@ import { Args, Int, Mutation, Resolver } from "@nestjs/graphql";
 import { Review } from "../models/review.model";
 import { ReviewService } from "../../services/review.service";
 import { ReviewInput } from "../models/review-input.model";
-import { UseGuards } from "@nestjs/common";
+import { Req, UseGuards } from "@nestjs/common";
 import { GraphqlJwtAuthGuard } from "../../../auth/graphql/guards/graphql-jwt-auth.guard";
+import { CurrentUser } from "../../../auth/helpers/current-user.helper";
 
 @UseGuards(GraphqlJwtAuthGuard)
 @Resolver(of => Review)
@@ -12,7 +13,8 @@ export class ReviewResolver {
 
   @Mutation(returns => Review, )
   createReview(@Args( { name: 'pizzaId', type: () => Int}) pizzaId: number,
-               @Args({ name: 'reviewInput', type: () => ReviewInput}) reviewInput: ReviewInput): Promise<Review> {
-    return this.reviewService.submitReview(pizzaId, { ...reviewInput, id: null, pizza: null, reviewedBy: 'Test' })
+               @Args({ name: 'reviewInput', type: () => ReviewInput}) reviewInput: ReviewInput,
+               @CurrentUser() user: any): Promise<Review> {
+    return this.reviewService.submitReview(pizzaId, { ...reviewInput, id: null, pizza: null, reviewedBy: user.username })
   }
 }
