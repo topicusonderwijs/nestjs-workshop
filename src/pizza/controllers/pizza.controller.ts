0da4399d2ecda2003249c39f12e4f2a1dbb1a647
tedroeloffzen <ted.roeloffzen@topicus.nl>
2022-11-28 11:41:48 +0100

    fix: few typo's and added parsing of query params

--- src/pizza/controllers/pizza.controller.ts
+++ src/pizza/controllers/pizza.controller.ts
@@ -5,8 +5,8 @@ import { Pizza } from '../../entities/pizza.entity';
 @Controller('/pizza')
 export class PizzaController {
     /**
-     * NestJS uses constructor dependency injection. So when a controller is created by NestJS it will lookup all constructor parameters
-     * as dependencies. It knows about dependencies trough its module definition (pizza.module.ts)
+     * NestJS uses constructor dependency injection. So when a controller is created by NestJS it will look up all constructor parameters
+     * as dependencies. It knows about dependencies through its module definition (pizza.module.ts)
      */
     constructor(private readonly pizzaService: PizzaService) {}
 
@@ -15,9 +15,10 @@ export class PizzaController {
         return this.pizzaService.getALlPizzas();
     }
 
+    // By default, query parameters are injected as strings, but we have to safely parse it to a number.
     @Get(':id')
-    public async getPizzaById(@Param('id') id: number): Promise<Pizza> {
-        const pizza = await this.pizzaService.getPizzaById(id);
+    public async getPizzaById(@Param('id') id: string): Promise<Pizza> {
+        const pizza = await this.pizzaService.getPizzaById(+id);
         if (!pizza) throw new NotFoundException(`Pizza with id: ${id} not found`);
         return pizza;
     }
--- src/pizza/controllers/review.controller.ts
+++ src/pizza/controllers/review.controller.ts
@@ -1,17 +1,18 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Post, UsePipes } from "@nestjs/common";
 import { ReviewService } from '../services/review.service';
 import { Review } from '../../entities/review.entity';
 
 @Controller('/review')
 export class ReviewController {
     /**
-     * NestJS uses constructor dependency injection. So when a controller is created by NestJS it will lookup all constructor parameters
-     * as dependencies. It knows about dependencies trough its module definition (pizza.module.ts)
+     * NestJS uses constructor dependency injection. So when a controller is created by NestJS it will look up all constructor parameters
+     * as dependencies. It knows about dependencies through its module definition (pizza.module.ts)
      */
     constructor(private readonly reviewService: ReviewService) {}
 
+    // By default, query parameters are injected as strings, but we have to safely parse it to a number.
     @Post(':pizzaId')
     public async submitReview(@Param('pizzaId') pizzaId: string, @Body() review: Review): Promise<Review> {
-        return this.reviewService.submitReview(parseInt(pizzaId), review);
+        return this.reviewService.submitReview(+pizzaId, review);
     }
 }
